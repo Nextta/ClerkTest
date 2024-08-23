a0446042a5bc868ed4af7883a9987db61bf6b1e0
Nextta <jairobernaldev@gmail.com>
2024-08-23 15:42:38 +0200

    Clerk config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import clerk from '@clerk/astro'
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk()],
+  output: "server",
+  adapter: cloudflare()
+});
