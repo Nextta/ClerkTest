73c16f9c68d63a22a265713c9360eaf188e95019
JairoDev <jairobernaldev@gmail.com>
2024-08-29 12:33:32 +0200

    Error corregido

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [clerk()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+ })
 });
